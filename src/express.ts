00d3b92b08e5add13dc7c2218b28e8194a47c356
Vili Ståhlberg <vili@sivulabra.fi>
2023-01-07 18:24:35 +0200

    Send some default HTML instead of 404 for Render.com health checks

--- src/express.ts
+++ src/express.ts
@@ -4,6 +4,8 @@ import { CONFIG } from "./config";
 export const startExpress = () => {
   const app = express();
 
+  app.get("/", (req, res) => res.send("Nothing to see here. 😜"));
+
   return app.listen(CONFIG.PORT, () =>
     console.log(`App listening on port ${CONFIG.PORT}!`)
   );
