0bb1887cff8db5e8d6ccb7328e0e32493d0712ee
Vili Ståhlberg <vili@sivulabra.fi>
2023-01-07 18:29:19 +0200

    Add custom health check path

--- src/express.ts
+++ src/express.ts
@@ -4,7 +4,13 @@ import { CONFIG } from "./config";
 export const startExpress = () => {
   const app = express();
 
+  // Root path "/"
   app.get("/", (req, res) => res.send("Nothing to see here. 😜"));
+
+  // Path used for health checking
+  app.get("/health", (req, res) =>
+    res.send("Everything appears to be in order! ✅")
+  );
 
   return app.listen(CONFIG.PORT, () =>
     console.log(`App listening on port ${CONFIG.PORT}!`)
